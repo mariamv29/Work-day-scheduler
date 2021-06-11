b58cdfac0842ea180886e4669f0a30bca6311d3f
Maria Mireles <mireles335@gmail.com>
2021-06-10 21:55:32 -0500

    html hour rows added

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+//Display today's date 
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDate").html(todayDate);
+
+var taskIdCounter =0;
+
+//Schedule times 
+var hourEl = 
+
+
+  
