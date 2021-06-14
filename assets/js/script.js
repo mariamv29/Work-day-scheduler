544fd36c7ed2fe33fcb815dda7fefc5a584cc6e4
Maria Mireles <mireles335@gmail.com>
2021-06-13 23:32:40 -0500

    adding save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDate").html(todayDate);
 
+//define DOM elements
 var currentHour = moment().hour(); //number
 console.log(currentHour);
 
@@ -9,28 +10,30 @@ var element = document.getElementById("time-block");
 var number = parseInt(element.getAttribute("data-index"));
 console.log(number);
 
+// for each function to loop / add css class
 function hourExpires() {
-  // for each function to loop through number array
-  $("time-block").each(function () {
+  $("#time-block").each(function () {
     if (currentHour === number) {
-      number.classList.add("present");
-      number.classList.remove("past");
-      number.classList.remove("future");
-    }
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+    } else if (currentHour > number) {
+      $(this).removeClass("present");
+      $(this).addClass("past");
+      $(this).removeClass("future");
+    } else $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
   });
 }
 
-
-
 //Adding saveBtn click listener
+$(function () {
+  $(".saveBtn").click(function () {
+    var text = $(this).val();
+    console.log(text);
+  });
+});
 
-// $(function () {
-//   $(".saveBtn").on("click", function () {
-//     var textarea = document.getElementById("textarea" + number).value;
-//     var time = $(this).parent().attr("id");
 
-//     //save task to local storage
-//     localStorage.setItem(time, input);
-//   });
-// });
 hourExpires();
