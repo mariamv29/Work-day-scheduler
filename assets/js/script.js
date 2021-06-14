7952d2334240bd490dd5dfd0bdef3767a8148e5f
Maria Mireles <mireles335@gmail.com>
2021-06-13 21:27:27 -0500

    testing css class with loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,35 @@
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDate").html(todayDate);
 
-var timeBlock = parseInt($(".time-block").attr("id"));
-function hourExpires() {
-  // create element past, future,
-  var currentHour = moment().hour(); //number
+var currentHour = moment().hour(); //number
+console.log(currentHour);
 
-  if (currentHour === timeBlock) {
-    $(this).addClass("present");
-    //   $(this).removeClass("past");
-    //   $(this).removeClass("future");
-  }
+var element = document.getElementById("time-block");
+var number = parseInt(element.getAttribute("data-index"));
+console.log(number);
+
+function hourExpires() {
+  // for each function to loop through number array
+  $("time-block").each(function () {
+    if (currentHour === number) {
+      number.classList.add("present");
+      number.classList.remove("past");
+      number.classList.remove("future");
+    }
+  });
 }
+
+
+
 //Adding saveBtn click listener
 
 // $(function () {
 //   $(".saveBtn").on("click", function () {
-//     var input = document.getElementById("textarea" + number).value;
+//     var textarea = document.getElementById("textarea" + number).value;
 //     var time = $(this).parent().attr("id");
 
 //     //save task to local storage
 //     localStorage.setItem(time, input);
 //   });
 // });
+hourExpires();
