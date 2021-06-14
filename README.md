315c745413685e39459fb7b27774b9927a29bf6e
Maria Mireles <mireles335@gmail.com>
2021-06-14 13:32:05 -0500

    testing localstorage

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # Work-day-scheduler
-To Create a work-day schedule, 9-5. Time blocks to reflect past, present and future time based on color code. 
-JavaScript/ JQuery used to complete functionality of color change, save task created and stored into local storage. 
+
+To Create a work-day schedule, 9-5. Time blocks to reflect past, present and future time based on color code.
+JavaScript/ JQuery used to complete functionality of color change, save task created and stored into local storage.
+
 ### HTML
-1. Timeblock HTML was added. 
+
+1. Timeblock HTML was added.
 
 ### JS
-1. Defined variables. 
-2. Set current day date. 
-3. Loop to fuction and add CSS Class. 
-3. Save button function. 
-4. Data saved to local storage. 
+
+1. Defined variables.
+2. Set current day date.
+3. Loop to fuction and add CSS Class.
+4. Save button function.
+5. Data saved to local storage.
 
 ### Page Screenshot:
+
 ![127 0 0 1_5500_index html](https://user-images.githubusercontent.com/83253575/121848285-73dae300-ccaf-11eb-8e63-3dac643304ef.png)
+
+### Deployment Link:
+
+https://mariamv29.github.io/Work-day-scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,28 +19,22 @@ var loadTasks = function () {
 
 //define DOM elements
 var currentHour = moment().hour(); //number
-console.log(currentHour);
 
 var element = document.getElementById("time-block");
 var number = parseInt(element.getAttribute("data-index"));
 
 // for each function to loop / add css class
-function hourExpires() {
-  $("#time-block").each(function () {
-    if (currentHour === number) {
-      $(this).addClass("present");
-      $(this).removeClass("past");
-      $(this).removeClass("future");
-    } else if (currentHour > number) {
-      $(this).removeClass("present");
-      $(this).addClass("past");
-      $(this).removeClass("future");
-      console.log(currentHour);
-      console.log(number);
-    } else $(this).removeClass("present");
+
+$("#time-block").each(function () {
+  if (currentHour === number) {
+    $(this).addClass("present");
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+  } else if (currentHour < number) {
+    $(this).removeClass("present");
     $(this).removeClass("past");
     $(this).addClass("future");
-  });
-}
-
-hourExpires();
+  } else $(this).removeClass("present");
+  $(this).addClass("past");
+  $(this).removeClass("future");
+});
