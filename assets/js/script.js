6bd7e27d5e0720b282852eb864b3455c82d61578
Maria Mireles <mireles335@gmail.com>
2021-06-14 14:34:17 -0500

    deploy link to readme/ localstorage works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
-var tasks = {};
-
 //Display today's date
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDate").html(todayDate);
 
-//Adding saveBtn click listener
-$("#time-block .saveBtn").click(function () {
-  //get form values
-  var input = $(".text").val();
-  var time = $("this").closest("div").attr("id");
-  //store
-  localStorage.setItem("input", input);
-});
+//Adding saveBtn
+$(document).ready(function () {
+  var saveBtn = $(".saveBtn").click(function () {
+    // define local storage variables
+    var strachpad = document.querySelector("#strachpad");
 
-var loadTasks = function () {
-  tasks = JSON.parse(localStorage.getItem("input", input));
-};
+    //get item- when refreshing page
+    strachpad.value = localStorage.getItem("notes");
+
+    //adding event listener
+    strachpad.addEventListener("keyup", (event) => {
+      localStorage.setItem("notes", strachpad.value);
+    });
+  });
+});
 
 //define DOM elements
 var currentHour = moment().hour(); //number
@@ -24,17 +25,18 @@ var element = document.getElementById("time-block");
 var number = parseInt(element.getAttribute("data-index"));
 
 // for each function to loop / add css class
-
-$("#time-block").each(function () {
-  if (currentHour === number) {
-    $(this).addClass("present");
-    $(this).removeClass("past");
+$(function () {
+  $("#time-block").each(function () {
+    if (currentHour === number) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+    } else if (currentHour < number) {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    } else $(this).removeClass("present");
+    $(this).addClass("past");
     $(this).removeClass("future");
-  } else if (currentHour < number) {
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-    $(this).addClass("future");
-  } else $(this).removeClass("present");
-  $(this).addClass("past");
-  $(this).removeClass("future");
+  });
 });
