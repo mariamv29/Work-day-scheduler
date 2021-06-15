c52f7b349948512e4b54e7ba8f8b961b560fc600
Maria Mireles <mireles335@gmail.com>
2021-06-14 23:30:00 -0500

    if statement works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,53 @@
-
 //Display today's date
 var todayDate = moment().format("dddd, MMM Do YYYY");
 $("#currentDate").html(todayDate);
 
-//Adding saveBtn{
- $(".saveBtn").click(function () {
+// define local storage variables
+var strachpad = document.querySelector("#strachpad");
 
-});
-
-// define local storage variables 
-var strachpad = document.querySelector("#strachpad")
-
-//get item- when refreshing page 
+//get item- when refreshing page
 strachpad.value = localStorage.getItem("notes");
 
-//adding event listener 
-strachpad.addEventListener("keyup", event => {
- localStorage.setItem("notes", strachpad.value)
-})
-
+//adding event listener
+strachpad.addEventListener("keyup", (event) => {
+  localStorage.setItem("notes", strachpad.value);
+});
 
 //define DOM elements
 var currentHour = moment().hour(); //number
 
-var element = document.getElementById("time-block");
-var number = parseInt(element.getAttribute("data-index"));
+var timeBlock = [
+  "time-block-9",
+  "time-block-10",
+  "time-block-11",
+  "time-block-12",
+  "time-block-13",
+  "time-block-14",
+  "time-block-15",
+  "time-block-16",
+  "time-block-17",
+];
 
+$.each(timeBlock, function (key, value) {
 
+  var element = document.getElementById(value);
+  console.log(element);
+  var number = parseInt(element.getAttribute("data-index"));
 
-// for each function to loop / add css class
-$(function () {
-$("#time-block").each(function () {
+  console.log(number, "number");
+  console.log(currentHour, "currentHour");
   if (currentHour === number) {
-    $(this).addClass("present");
-    $(this).removeClass("past");
-    $(this).removeClass("future");
-  } else if (currentHour < number) {
-    $(this).removeClass("present");
-    $(this).removeClass("past");
-    $(this).addClass("future");
-  } else $(this).removeClass("present");
-  $(this).addClass("past");
-  $(this).removeClass("future");
-})
+    $(element).addClass("present");
+    $(element).removeClass("past");
+    $(element).removeClass("future");
+  } else if 
+  (currentHour > number) {
+    $(element).removeClass("present");
+    $(element).addClass("past");
+    $(element).removeClass("future");
+  } else {
+    $(element).removeClass("present");
+    $(element).removeClass("past");
+    $(element).addClass("future");
+  }
 });
